06d956b9f925f2cf54ae8c2d331833389bd485f4
teddyjung8808 <teddyjung8808@gmail.com>
2022-05-15 13:28:41 +0900

    add: 카테고리 생성 및 카테고리별 포스트 아이템 보여주기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useMemo } from 'react'
 import { graphql } from 'gatsby'
+import queryString, { ParsedQuery } from 'query-string'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
 
 import styled from '@emotion/styled'
 import GlobalStyle from '@Components/Common/GlobalStyle'
 import Introduction from '@Components/Main/Introduction'
-import CategoryList from '@Components/Main/CategoryList'
+import CategoryList, { CategoryListProps } from '@Components/Main/CategoryList'
 import PostList from '@Components/Main/PostList'
 import Footer from '@Components/Common/Footer'
 import { PostListItemType } from 'types/PostItem.types'
 
 type IndexPageProps = {
+  location: {
+    search: string
+  }
   data: {
     allMarkdownRemark: {
       edges: PostListItemType[]
@@ -24,6 +28,7 @@ type IndexPageProps = {
 }
 
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  location: { search },
   data: {
     allMarkdownRemark: { edges },
     file: {
@@ -31,12 +36,49 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
     },
   },
 }) {
+  const parsed: ParsedQuery<string> = queryString.parse(search)
+
+  console.log('parsed :', parsed)
+
+  const selectedCategory: string =
+    typeof parsed.category !== 'string' || !parsed.category
+      ? 'All'
+      : parsed.category
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list: CategoryListProps['categoryList'],
+          {
+            node: {
+              frontmatter: { categories },
+            },
+          }: PostListItemType,
+        ) => {
+          categories.forEach(category => {
+            if (list[category] === undefined) list[category] = 1
+            else list[category]++
+          })
+
+          list['All']++
+
+          return list
+        },
+        { All: 0 },
+      ),
+    [],
+  )
+
   return (
     <Container>
       <GlobalStyle />
       <Introduction profileImage={gatsbyImageData} />
-      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-      <PostList posts={edges} />
+      <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={categoryList}
+      />
+      <PostList selectedCategory={selectedCategory} posts={edges} />
       <Footer />
     </Container>
   )
@@ -47,12 +89,6 @@ const Container = styled.div`
   flex-direction: column;
   height: 100vh;
 `
-
-const CATEGORY_LIST = {
-  All: 5,
-  Web: 3,
-  Mobile: 2,
-}
 
 export const getPostList = graphql`
   query getPostList {
